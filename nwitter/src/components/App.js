134b4fb48cb9e2174e5c6734f3041ddd2193f821
김주원 <kjwt1124@icloud.com>
2022-01-28 17:19:09 +0900

    #2.0 Using Firebase Auth

---
+++ nwitter/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { useState}  from "react"
+import { authService } from "fbase";
+import AppRouter from "components/Router";
+
+function App() {
+  
+  const [ isLoggedIn, setIsLoggedin ] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>
+      <footer>&copy; {new Date().getFullYear} Nwitter </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ nwitter/src/routes/Profile.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const Profie = () => <span>Profie</span>;
+export default Profile
