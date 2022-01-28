7dd50cc6737306fcc51c20297bfd7cee49c17214
김주원 <kjwt1124@icloud.com>
2022-01-28 18:33:38 +0900

    #2.2 Creating Account

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { authService } from "../fbase";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
     const onChange = (event) =>{
         const {
             target : { name, value}
@@ -14,8 +16,15 @@ const Auth = () => {
         }
 
     };
-    const onSubmit = (event)=>{
+    const onSubmit = async(event)=>{
         event.preventDefault();
+        if (newAccount) {
+            // create account
+            await authService.createUserWithEmailAndPassword(email, password);
+        } else {
+            // log in 
+            await authService.signInWithEmailAndPassword(email, password);
+        }
     }
     return(
         <div>
@@ -29,14 +38,14 @@ const Auth = () => {
                     onChange={onChange}
                 />
                 <input 
-                    name = "text"
-                    type="text" 
+                    name = "password"
+                    type="password" 
                     placeholder="Password" 
                     required
                     value = {password}
                     onChange={onChange}
                 />
-                <input type="submit" value = "Log In" />
+                <input type="submit" value = { newAccount ? "Create Account" : "Log In"} />
             </form>
             <div>
                 <button>Continue with Google</button>
