9ae95cdb010fabb454d2f17ae1ec10c3d40a504c
김주원 <kjwt1124@icloud.com>
2022-01-28 22:32:14 +0900

    Updates

---
+++ precache-manifest.cea93dabcd2a2f88313128a952e1270f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6e33868904d3172938d2dd541cde59",
+    "url": "/nwitter.git/index.html"
+  },
+  {
+    "revision": "b5c8dad58ccace22480a",
+    "url": "/nwitter.git/static/css/main.b4eb3f7c.chunk.css"
+  },
+  {
+    "revision": "7d7fc24c6227bc362a9a",
+    "url": "/nwitter.git/static/js/2.49aa17db.chunk.js"
+  },
+  {
+    "revision": "af5b42ac6a47ebea58e56b2b1546c7e8",
+    "url": "/nwitter.git/static/js/2.49aa17db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5c8dad58ccace22480a",
+    "url": "/nwitter.git/static/js/main.2608f4d6.chunk.js"
+  },
+  {
+    "revision": "b8d94dd6c70e98c49543",
+    "url": "/nwitter.git/static/js/runtime-main.2ba7d70b.js"
+  }
+]);
